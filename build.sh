ab777b344897e1c4be0b9ee14164aafc87697fc4
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2020-12-01 13:40:03 +0330

    add builder

---
+++ build.sh
@@ -0,0 +1,15 @@
+docker rm -f ssh_tunnel
+docker image rm ssh_tunnel
+git-crypt unlock /etc/cvsc/secret
+find . -type f -print0 | xargs -0 dos2unix --
+docker build -t ssh_tunnel --no-cache --build-arg TUNNEL_USER=tunnel --build-arg LISSTEN_ADDR=sshtunnel .
+# create Container
+docker create \
+  --cap-add=NET_ADMIN \
+  --name=ssh_tunnel \
+  --network=cwn \
+  --privileged \
+  --restart unless-stopped \
+  ssh_tunnel
+
+docker start ssh_tunnel && docker logs ssh_tunnel -f
